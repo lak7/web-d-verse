27e5055b264d2281a52a9fc1c39921f6202be8c7
Lakshay Gupta <lakshaygupta2511@gmail.com>
2023-12-03 17:17:42 +0530

    changed some content

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -39,9 +39,6 @@ const Header = ({ t }: { t: TFunction }) => {
         <CustomNavLinkSmall onClick={() => scrollTo("mission")}>
           <Span>{t("Mission")}</Span>
         </CustomNavLinkSmall>
-        <CustomNavLinkSmall onClick={() => scrollTo("product")}>
-          <Span>{t("Product")}</Span>
-        </CustomNavLinkSmall>
         <CustomNavLinkSmall
           style={{ width: "180px" }}
           onClick={() => scrollTo("contact")}
